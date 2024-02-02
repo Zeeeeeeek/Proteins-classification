f4602575061f239e85b9caba5ec9694d1163dcf9
Enrico Ulissi <enricoulissi01@gmail.com>
2024-02-02 10:20:18 +0100

    reformat and add pdb api call

--- api.py
+++ api.py
@@ -1,8 +1,14 @@
 import requests
 
-URL = "https://repeatsdb.bio.unipd.it/api//search"
+REPEATSDB_URL = "https://repeatsdb.bio.unipd.it/api//search"
+PDB_URL = "https://files.rcsb.org/download/"
 
 
-def query_api(query):
-    r = requests.get(URL, params=query)
+def repeatsdb_get(query):
+    r = requests.get(REPEATSDB_URL, params=query)
     return r.json()
+
+
+def pdb_get(pdb_id):
+    r = requests.get(PDB_URL + pdb_id + ".pdb")
+    return r.text
