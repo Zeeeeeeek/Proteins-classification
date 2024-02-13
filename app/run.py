6b2e5597469fdd543973c784b241101de801d8a0
Enrico Ulissi <enricoulissi01@gmail.com>
2024-02-13 18:51:38 +0100

    remove folder creation

--- app/run.py
+++ app/run.py
@@ -1,13 +1,7 @@
-import os
-
 from cli import handle_cli_input
 
 
 def main():
-    if not os.path.exists("../csv"):
-        os.makedirs("../csv")
-    if not os.path.exists("../kmers"):
-        os.makedirs("../kmers")
     handle_cli_input()
 
 
