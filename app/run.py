c7ec277839074ceac7a1d8683a59b7f190679f41
Enrico Ulissi <enricoulissi01@gmail.com>
2024-02-14 14:49:34 +0100

    log exceptions

--- app/run.py
+++ app/run.py
@@ -1,8 +1,14 @@
 from cli import handle_cli_input
+import logging
+
+logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 
 def main():
-    handle_cli_input()
+    try:
+        handle_cli_input()
+    except Exception as e:
+        logging.error(e)
 
 
 if __name__ == '__main__':
