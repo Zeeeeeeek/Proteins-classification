a9ad5b9fe0de83e8d1d3523a0dd8bf0a7b5536c7
Enrico Ulissi <enricoulissi01@gmail.com>
2024-02-15 11:58:55 +0100

    different changes

--- app/kmer.py
+++ app/kmer.py
@@ -7,7 +7,9 @@ import pandas as pd
 from data_preprocessing import split_df_into_n
 import logging
 
-logging.basicConfig(filename='kmer.log', level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(filename='kmer.log', level=logging.INFO, format='%(asctime)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
+
 
 def kmer_count(k, sequence):
     """
@@ -52,32 +54,32 @@ def kmer_count_dataframe(k, df):
 def multithread_kmer_count_df(df_path, k, output_path, n_threads=5):
     df = pd.read_csv(df_path)
     dfs = split_df_into_n(df, 5 if n_threads < 5 else n_threads)
-
-    def worker_function(sdf, worker_k, result_list):
-        result_list.append(kmer_count_dataframe(worker_k, sdf))
-
     count = 0
-    for sub_df in dfs:
-        chunks = split_df_into_n(sub_df, 5 if n_threads < 5 else n_threads)
-        chunk_threads = []
-        merged_dfs = []
-        logging.info(f"Threads count: {len(chunks)} merged_dfs count: {len(merged_dfs)}")
-        logging.info(f"Subdf shape: {sub_df.shape}")
-        for chunk in chunks:
-            logging.info(f"Processing chunk {count}")
-            logging.info(f"Chunk shape: {chunk.shape}")
-            t = threading.Thread(target=worker_function, args=(chunk, k, merged_dfs))
-            chunk_threads.append(t)
-            t.start()
-        for t in chunk_threads:
-            t.join()
-        merged_df = pd.concat(merged_dfs, ignore_index=True)
-        merged_df.to_csv(f"cache_{count}.csv", index=False)
-        logging.info(f"Subdf {count} processed")
-        count += 1
+    for index, sub_df in enumerate(dfs):
+        ext(sub_df, index, k, n_threads)
+        count = index + 1
     logging.info(f"Merging {count} files")
     merge_csv_files(output_path, count, 100)
 
+
+def ext(df, df_index, k, n_threads=5):
+    def worker_function(sdf, worker_k, result_list):
+        result_list.append(kmer_count_dataframe(worker_k, sdf))
+
+    chunks = split_df_into_n(df, 5 if n_threads < 5 else n_threads)
+    chunk_threads = []
+    merged_dfs = []
+    logging.info(f"Subdf shape: {df.shape}")
+    for chunk in chunks:
+        logging.info(f"Chunk shape: {chunk.shape}")
+        t = threading.Thread(target=worker_function, args=(chunk, k, merged_dfs))
+        chunk_threads.append(t)
+        t.start()
+    for t in chunk_threads:
+        t.join()
+    merged_df = pd.concat(merged_dfs, ignore_index=True)
+    merged_df.to_csv(f"cache_{df_index}.csv", index=False)
+    logging.info(f"Cached subdf {df_index}")
 
 def merge_csv_files(output_path, count, CHUNK_SIZE):
     for i in range(count):
