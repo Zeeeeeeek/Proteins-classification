eb07781678800aac0c58e013ef24c539281a620b
Enrico Ulissi <enricoulissi01@gmail.com>
2024-02-16 11:49:07 +0100

    Revert "fix .csv duplication"
    This reverts commit 7632836d7d08ccb66877fc90f2dd57df20e7ff6e.

--- app/controller.py
+++ app/controller.py
@@ -23,7 +23,5 @@ def run_query(query_string, file_name, merge_regions, n_threads):
 
 def run_kmer_count(input_file, k, output_file, n_threads=5):
     input_name = input_file if input_file.endswith(".csv") else input_file + ".csv"
-    output_name = output_file if output_file is not None else input_file + "_" + str(k) + "_mer.csv"
-    if not output_name.endswith(".csv"):
-        output_name = output_name + ".csv"
+    output_name = output_file + ".csv" if output_file is not None else input_file + "_" + str(k) + "_mer.csv"
     multithread_kmer_count_df(input_name, k, output_name, n_threads)
