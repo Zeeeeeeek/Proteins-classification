684c54e38a7f6b2573e510b82d595533bcae9611
Enrico Ulissi <enricoulissi01@gmail.com>
2024-02-20 17:19:01 +0100

    remove logging

--- app/run.py
+++ app/run.py
@@ -1,14 +1,10 @@
 from cli import handle_cli_input
-import logging
-
-logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-
 
 def main():
     try:
         handle_cli_input()
     except Exception as e:
-        logging.error(e)
+        print(e)
 
 
 if __name__ == '__main__':
