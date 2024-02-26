b50afa5f925dc8a5a8f1857c6720234eeb374885
Enrico Ulissi <enricoulissi01@gmail.com>
2024-02-26 23:27:49 +0100

    change multithread handling

--- app/kmer.py
+++ app/kmer.py
@@ -1,5 +1,5 @@
 import concurrent.futures
-import threading
+import queue
 import pandas as pd
 
 from app.KmerCsvWriter import KmerCsvWriter
@@ -39,17 +39,24 @@ def single_thread_kmer_count(df_path, k: int, output_path):
 def kmer_count_multithread(df_path, k, output_path, n_threads):
     df = pd.read_csv(df_path, usecols=["region_id", "class_topology_fold_clan", "sequence"])
     writer = KmerCsvWriter(output_path)
+    q = queue.Queue()
 
     def worker_function(worker_row, worker_k):
-        return (
-            kmer_count(worker_k, worker_row['sequence']), worker_row['region_id'],
-            worker_row['class_topology_fold_clan'],
-            worker_row['sequence']
+        q.put(
+            (
+                kmer_count(worker_k, worker_row['sequence']), worker_row['region_id'],
+                worker_row['class_topology_fold_clan'],
+                worker_row['sequence']
+            )
         )
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=n_threads) as executor:
-        future_to_row = {executor.submit(worker_function, row, k): row for _, row in df.iterrows()}
-        for future in concurrent.futures.as_completed(future_to_row):
-            tup = future.result()
-            writer.write_kmer_count(tup[0], tup[1], tup[2], tup[3])
+        for _, row in df.iterrows():
+            executor.submit(worker_function, row, k)
+        q.put(None)
+    while True:
+        t = q.get()
+        if t is None:
+            break
+        writer.write_kmer_count(t[0], t[1], t[2], t[3])
     writer.close()
