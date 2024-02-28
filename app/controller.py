00dde40fa16f99a5d414d44ab1a5c16219ab359a
Enrico Ulissi <enricoulissi01@gmail.com>
2024-02-28 10:28:56 +0100

    remove unused regex control

--- app/controller.py
+++ app/controller.py
@@ -1,16 +1,8 @@
-import re
 from typing import List
 
 from app.repeats import query_repeatsdb_to_csv
 from app.kmer import single_thread_kmer_count
 from app.models import run_models
-
-COLON_PATTERN = re.compile(r'^(?:[^:]+:)+[^:]+$')
-
-
-def check_query_string(query_string):
-    if not COLON_PATTERN.match(query_string):
-        raise ValueError("Query string must be in the form of \"field:value\".")
 
 
 def run_repeatsdb_query(query_classes: List[str], file_name, merge_regions, n_threads):
