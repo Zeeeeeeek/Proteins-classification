01b68966dc693a5ad961dbfc63e1187ac4a910b3
Enrico Ulissi <enricoulissi01@gmail.com>
2024-03-01 15:15:14 +0100

    add toolbar

--- gui/gui.py
+++ gui/gui.py
@@ -1,5 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QStackedWidget, \
-    QHBoxLayout, QSpacerItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QStackedWidget, QToolBar, QGridLayout, QToolButton
 
 from gui.KmerWidget import KmerCountWidget
 from gui.ModelsWidget import ModelsWidget
@@ -13,44 +12,40 @@ class MainWindow(QMainWindow):
         self.query_widget = QueryWidget(self)
         self.models_widget = ModelsWidget(self)
 
-        self.kmer_button = QPushButton("Kmer")
+        # Toolbar
+        self.toolbar = QToolBar()
+        self.kmer_button = QToolButton()
+        self.kmer_button.setText("Kmer")
+        self.kmer_button.setAutoExclusive(True)
         self.kmer_button.clicked.connect(self.show_kmer_widget)
-        self.query_button = QPushButton("Query")
+        self.query_button = QToolButton()
+        self.query_button.setText("Query")
+        self.query_button.setAutoExclusive(True)
         self.query_button.clicked.connect(self.show_query_widget)
-        self.models_button = QPushButton("Models")
+        self.models_button = QToolButton()
+        self.models_button.setText("Models")
+        self.models_button.setAutoExclusive(True)
         self.models_button.clicked.connect(self.show_models_widget)
+        self.toolbar.addWidget(self.query_button)
+        self.toolbar.addWidget(self.kmer_button)
+        self.toolbar.addWidget(self.models_button)
 
+        # Stacked widget
         self.stacked_widget = QStackedWidget()
         self.stacked_widget.addWidget(self.query_widget)
         self.stacked_widget.addWidget(self.kmer_widget)
         self.stacked_widget.addWidget(self.models_widget)
 
-        button_layout = QHBoxLayout()
+        # Grid layout
+        layout = QGridLayout()
+        layout.addWidget(self.toolbar, 1, 1)
+        layout.addWidget(self.stacked_widget, 2, 1)
 
-        left_spacer = QSpacerItem(10, 1)
-        button_layout.addItem(left_spacer)
+        # Central widget
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
-        button_layout.addWidget(self.query_button)
-
-        button_layout.addSpacing(10)
-
-        button_layout.addWidget(self.kmer_button)
-
-        button_layout.addSpacing(10)
-
-        button_layout.addWidget(self.models_button)
-
-        right_spacer = QSpacerItem(10, 1)
-        button_layout.addItem(right_spacer)
-
-        layout = QVBoxLayout()
-        layout.addLayout(button_layout)
-        layout.addWidget(self.stacked_widget)
-
-        self.central_widget = QWidget()
-        self.central_widget.setLayout(layout)
-
-        self.setCentralWidget(self.central_widget)
         self.setWindowTitle("Protein Classifier")
         self.setFixedSize(400, 350)
 
