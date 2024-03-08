4766fc4f4bd3376750f0c3a99ea2b1d570342bd9
Enrico Ulissi <enricoulissi01@gmail.com>
2024-03-08 19:26:10 +0100

    multiple changes

--- app/models.py
+++ app/models.py
@@ -3,12 +3,14 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn import metrics
-from sklearn.cluster import AgglomerativeClustering, DBSCAN, AffinityPropagation, MeanShift
+from sklearn.cluster import AgglomerativeClustering, AffinityPropagation, MeanShift
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler, Normalizer
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
@@ -82,6 +84,29 @@ def get_classifiers_results(X, y, random_state):
     return results
 
 
+def get_classifiers_results_with_k_fold(X, y, random_state):
+    names, models = get_classifiers(random_state)
+    scores = {}
+
+    for name, model in zip(names, models):
+        scoring = {
+            "Accuracy": make_scorer(metrics.accuracy_score),
+            "Precision": make_scorer(metrics.precision_score, average='weighted', zero_division=1),
+            "Recall": make_scorer(metrics.recall_score, average='weighted', zero_division=1),
+            "F1 score": make_scorer(metrics.f1_score, average='weighted')
+        }
+        cv_results = cross_validate(model, X, y, cv=5, scoring=scoring)
+
+        scores[name] = {
+            "Accuracy": cv_results['test_Accuracy'].mean(),
+            "Precision": cv_results['test_Precision'].mean(),
+            "Recall": cv_results['test_Recall'].mean(),
+            "F1 Score": cv_results['test_F1 score'].mean()
+        }
+
+    return scores
+
+
 def print_results(results):
     for classifier, met in results.items():
         print("=" * 30 + "\n")
@@ -95,21 +120,10 @@ def print_results(results):
         print()
 
 
-def print_k_fold_results(X, y, random_state):
-    cv = KFold(n_splits=5, random_state=random_state, shuffle=True)
-    names, classifiers = get_classifiers(random_state)
-    print("K-Fold Cross Validation Results:")
-    for index, classifier in enumerate(classifiers):
-        print("=" * 30 + "\n")
-        print(names[index])
-        scores = cross_val_score(classifier, X, y, cv=cv, scoring='accuracy')
-        print(f"Accuracy: {scores.mean():.4f}")
-        print()
-
-
-def print_cluster_metrics(labels_true, labels_pred, method: str):
+def print_cluster_metrics(labels_true, labels_pred, method: str, data, random_state):
     print("=" * 30)
     print(f"{method}")
+    print("\tSilhouette_score", metrics.silhouette_score(data, labels_pred, random_state=random_state))
     print("\tRand_score", metrics.rand_score(labels_true, labels_pred))
     print("\tHomogeneity_score", metrics.homogeneity_score(labels_true, labels_pred))
     print("\tCompleteness_score", metrics.completeness_score(labels_true, labels_pred))
@@ -117,22 +131,22 @@ def print_cluster_metrics(labels_true, labels_pred, method: str):
 
 def clustering(X, labels, random_state):
     clusters = [
-        AgglomerativeClustering(n_clusters=len(labels)),
-        DBSCAN(),
+        AgglomerativeClustering(n_clusters=len(set(labels))),
+        # DBSCAN(),
         AffinityPropagation(random_state=random_state),
         MeanShift()
     ]
 
     names = [
         "Agglomerative Clustering",
-        "DBSCAN",
+        # "DBSCAN",
         "Affinity Propagation",
         "Mean Shift"
     ]
 
     for index, model in enumerate(clusters):
         model.fit(X)
-        print_cluster_metrics(labels, model.labels_, names[index])
+        print_cluster_metrics(labels, model.labels_, names[index], X, random_state)
 
 
 def get_sampled_regions(df_path, level: str, sample_size: int, random_state):
@@ -173,14 +187,63 @@ def read_csv_of_regions(df_path, regions, k):
                     else:
                         row.append(split[i])
                 rows.append(row)
-                try:
-                    reg_copy.remove(split[0])
-                except ValueError:
-                    print(f"Error: region {split[0]} not found in regions list.")
-                    raise
+                reg_copy.remove(split[0])
             if len(reg_copy) == 0:
                 break
-    return pd.DataFrame(rows, columns=columns)
+    print("\tCreating DataFrame...")
+    df = pd.DataFrame(rows, columns=columns)
+    # print(df.shape)
+    # to_drop = []
+    # for col in df.columns[3:]:
+    #    if df[col].sum() == 0:
+    #        to_drop.append(col)
+    # df.drop(columns=to_drop, inplace=True)
+    # print(df.shape)
+    # df.to_csv("test.csv", index=False)
+    # Standardize data
+    # print("\tStandardizing data...")
+    # scaler = StandardScaler()
+    # df[df.columns[3:]] = scaler.fit_transform(df[df.columns[3:]])
+    # print("\tNormalizing data...")
+    # normalizer = Normalizer()
+    # df[df.columns[3:]] = normalizer.fit_transform(df[df.columns[3:]])
+
+    return df
+
+
+def get_best_classifier(results):
+    max_dict = {
+        "Accuracy": 0,
+        "Precision": 0,
+        "Recall": 0,
+        "F1 Score": 0
+    }
+    for classifier, met in results.items():
+        for metric, value in met.items():
+            if value > max_dict[metric]:
+                max_dict[metric] = value
+    scores = {}  # Each classifier with the best metric gains a point
+    for classifier, met in results.items():
+        for metric, value in met.items():
+            if value == max_dict[metric]:
+                if classifier in scores:
+                    scores[classifier] += 1
+                else:
+                    scores[classifier] = 1
+    # Sort by points
+    sorted_scores = dict(sorted(scores.items(), key=lambda item: item[1], reverse=True))
+    result = []
+    best_found = False
+    for classifier, points in sorted_scores.items():
+        if not best_found:
+            result.append(classifier)
+            best_found = True
+        else:
+            if points == list(sorted_scores.values())[0]:
+                result.append(classifier)
+            else:
+                break
+    return result
 
 
 def run_models(df_path, level, method, max_sample_size_per_level, k, random_state=42):
@@ -194,17 +257,16 @@ def run_models(df_path, level, method, max_sample_size_per_level, k, random_stat
     print("\tReading data...")
     df = read_csv_of_regions(df_path, sampled_regions, k)
     y, X = get_y_and_X(df, level)
+    print(len(y))
     if 'sequence' in X.columns:
         X.drop(columns=['sequence'], inplace=True)
-    print("Data read.")
     if method == 'clustering':
         print("Clustering...")
         clustering(X, y, random_state)
     else:
         print("Running classifiers...")
-        results = get_classifiers_results(X, y, random_state)
-        print_results(results)
-        print_k_fold_results(X, y, random_state)
+        print_results(get_classifiers_results_with_k_fold(X, y, random_state))
+        print("Best classifier(s):", get_best_classifier(get_classifiers_results_with_k_fold(X, y, random_state)))
     print("Done.")
 
 
